c981bf8dcaa783946b0de29b53205123bfa9f79e
Roberto Rodríguez <krowno.iso@gmail.com>
2022-05-23 09:39:34 +0200

    efectuados cambios para despliegue en heroku

--- yugiAPI.py
+++ yugiAPI.py
@@ -27,6 +27,5 @@ def get_info(palabra):
 get_info("Tornado%20Dragon")
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+port = int(os.environ.get('PORT', 5000))
+app.run(debug=True, host='0.0.0.0', port=port)
